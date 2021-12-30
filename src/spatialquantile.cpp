3d72eabff758945cbe4dce1c9edbcb8df28f1110
Joydeep Chowdhury <66551529+joydeepchowdhury@users.noreply.github.com>
2021-12-30 20:25:06 +0300

    translation 2

--- src/spatialquantile.cpp
+++ src/spatialquantile.cpp
@@ -8,7 +8,9 @@ double g_function_weighted(arma::mat X_local, arma::vec Q_local, arma::vec weigh
   double g;
   
   int n = X_local.nrow();
-  int q = X_local.ncol();
+  int d = X_local.ncol();
+  
+  
   
   g = sum(weights_local .* ( sum(( (ones(size(X_local,1), 1) * Q_local) -...
     X_local ).^2, 2) ).^(1/2), 1) / sum(weights_local) - u_local * Q_local';
